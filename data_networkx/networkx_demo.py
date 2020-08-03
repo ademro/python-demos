34d44d4740a91f516f33e0619c0d97c6a50a1780
Aaron Demro <aarondemro@Aarons-MacBook-Pro.local>
2020-08-03 07:03:52 -0600

    code cleanup

--- data_networkx/networkx_demo.py
+++ data_networkx/networkx_demo.py
@@ -5,39 +5,33 @@ import pandas as pd
 import networkx as nx
 import matplotlib
 
-#Load the data -- courtesy of https://www.baseball-reference.com/teams/MIN/2019-schedule-scores.shtml
+#Load the data courtesy of https://www.baseball-reference.com/teams/MIN/2019-schedule-scores.shtml
 data=util.loadCSVfile('Twins2019Schedule.csv')
-opacity_table = util.loadCSVfile('hex_opacity_table.csv')
 
-#instantiate the Network X graph object
+#Instantiate the Network X graph object.
 G=nx.Graph()
 
-#Add a node for the Twins -- I am going to make it blue so it is distinguished.  Color is added as a property of the node.  You will see why this is important when the network diagram is rendered.
+#Add a node for the Twins.  I am going to make it blue and a size of 500 so it is distinguished.  Color is added as a property of the node.  You will see why this is important and used in laters steps to provide color to the image.
 
 G.add_node('MIN',color='#0000FF',size=500)
 
-#create weights lookup table to distinguish Teams played more in the schedule.  This will naturally depict in the graph by the distance from the central node.  The more times the opposing Team plays the Twins, the closer they will reside to MIN.  To make even more visually easier to see, the size of each entry will also be proportional to the number of games played.
+#Create a weights lookup table to distinguish teams played more in the schedule.  This will naturally depict in the graph by the distance from the central node.  The more times the opposing Team plays the Twins, the closer they will reside to MIN on the network map.  To make even more visually easier to see, the size of each entry will also be proportional to the number of games played.
+
+#To make that possible to turn the column of the data 'Opp' to a data dictionary indexed by Team.
 
 opponent_dic = data['Opp'].value_counts().to_dict()
 
-#List the percentage games played against the Twins in a dictionary
-
-opponent_percent = {}
-for i in opponent_dic:
-    opponent_percent[i]=round((opponent_dic[i]/162)*100)
-
-#Add nodes for the opposing Teams.  To make it easier to see I've added opposing Teams ad red. The size of the nodes gets larger proportional to the number of games played against the Twins.  In other words, larger dot, more games played.
+#Add nodes for the opposing teams. To make it easier to see I've added opposing Teams as red and made the size of the nodes proportional to the number of games played against the Twins.  In other words, a larger dot equates more games played.
 
 for i in range(len(data)):
     G.add_node(data.iloc[i,5], color='red',size=opponent_dic[data.iloc[i,5]]*100)
 
-
-#Add the connections or 'edges' to the network diagram.
+#Add the connections or 'edges' to the network diagram.  Similar to the size of the node, the distance of the node from the center is proportional to the number of games played against the Twins.  This is done by assigning a property 'weight' to each edge.  The closer the node to the center, the more games played.
 
 for i in range(len(data)):
     G.add_edges_from([(data.iloc[i,3],data.iloc[i,5],{'weight': opponent_dic[data.iloc[i,5]]})])
 
-#assign colors and node sizes.  This iterates through each node object and creates a list showing the colors and sizes cooresponding to the in the G object.  Given the index of the list matches the index of the object, when we render the image in later steps the color and sizes are assigned to the correct node.
+#Create list of assigned node sizes and colors. This step iterates through each node object and creates a list showing the node colors and sizes.  Given the index of the list matches the index of the nodes in the G object, when we render the image in the next step the color and sizes are assigned to the correct node.
 
 node_color=[]
 node_size=[]
@@ -47,6 +41,6 @@ for node in G.nodes():
     size = G.nodes[node]['size']
     node_size.append(size)
 
-#draw the network digram!
+#Draw the network digram assigning node_color and node_size using the lists established in the previous step.
 
 nx.draw(G, with_labels=True,node_color= node_color,node_size=node_size)
