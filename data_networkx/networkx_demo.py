627ba73bf58d09c4bbe2640692a66b32cf1deedc
Aaron Demro <aarondemro@Aarons-MacBook-Pro.local>
2020-08-03 23:05:45 -0600

    minor text alterations

--- data_networkx/networkx_demo.py
+++ data_networkx/networkx_demo.py
@@ -17,11 +17,9 @@ G.add_node('MIN',color='#0000FF',size=500)
 
 #Create a weights lookup table to distinguish teams played more in the schedule.  This will naturally depict in the graph by the distance from the central node.  The more times the opposing Team plays the Twins, the closer they will reside to MIN on the network map.  To make even more visually easier to see, the size of each entry will also be proportional to the number of games played.
 
-#To make that possible to turn the column of the data 'Opp' to a data dictionary indexed by Team.
-
 opponent_dic = data['Opp'].value_counts().to_dict()
 
-#Add nodes for the opposing teams. To make it easier to see I've added opposing Teams as red and made the size of the nodes proportional to the number of games played against the Twins.  In other words, a larger dot equates more games played.
+#Add nodes for the opposing teams. To make it easier to see I've added opposing teams as red and made the size of the nodes proportional to the number of games played against the Twins using the opponent_dic lookup table.  In other words, a larger dot equates more games played.
 
 for i in range(len(data)):
     G.add_node(data.iloc[i,5], color='red',size=opponent_dic[data.iloc[i,5]]*100)
@@ -41,6 +39,6 @@ for node in G.nodes():
     size = G.nodes[node]['size']
     node_size.append(size)
 
-#Draw the network digram assigning node_color and node_size using the lists established in the previous step.
+#Draw the network digram assigning node_color and node_size using the lists established in the previous step and lavish in your new found network diagram greatness.
 
 nx.draw(G, with_labels=True,node_color= node_color,node_size=node_size)
